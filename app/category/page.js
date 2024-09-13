de190bfc1ab485e6c949bfa9a4ca59596bb94339
Pyae Phyo Aung (Austin) <austin.xu.me@gmail.com>
2024-09-13 14:09:42 +0700

    change api base

--- app/category/page.js
+++ app/category/page.js
@@ -6,9 +6,10 @@ import Link from "next/link";
 export default function Home() {
   const [categoryList, setCategoryList] = useState([]);
   const { register, handleSubmit } = useForm();
+  const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
   async function fetchCategory() {
-    const data = await fetch("http://localhost:3000/api/category");
+    const data = await fetch(`${API_BASE}/category`);
     const c = await data.json();
     setCategoryList(c);
   }
@@ -18,7 +19,7 @@ export default function Home() {
   }, []);
 
   function createCategory(data) {
-    fetch("http://localhost:3000/api/category", {
+    fetch(`${API_BASE}/category`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
